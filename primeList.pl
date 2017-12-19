e139f75ad134ddbdba915db8df1c94c3d1e5e095
Dan Choe <devdanchoe@gmail.com>
2017-12-19 01:47:21 -0500

    Generate a list of N prime numbers

---
+++ primeList.pl
@@ -0,0 +1,26 @@
+is_prime(2).
+is_prime(3).
+is_prime(N) :-
+    N > 3,
+    is_prime(N, 2).
+is_prime(N, X):-
+    N =:= X.
+is_prime(N, X):-
+    R is N mod X,
+    R \= 0,
+    X1 is X + 1,
+    is_prime(N, X1).
+
+make_NprimeList(N, L):-
+	make_NprimeList(N, 0, 2, [], L).
+
+make_NprimeList(N, N, _, Temp, Temp).
+make_NprimeList(N, CurrNth, P, Temp, L):-
+    N > CurrNth,
+    P1 is P + 1,
+    ( 
+    	is_prime(P)
+    	->  CurrNth1 is CurrNth + 1,
+        make_NprimeList(N, CurrNth1, P1, [P|Temp], L)
+    	; make_NprimeList(N, CurrNth, P1, Temp, L)
+    ).
